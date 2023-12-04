a26943d1fd3066013cb918d87b267a3e2aa14b29
ADITYA2777 <ajadityajain01@gmail.com>
2023-12-04 11:01:14 -0800

    complete project

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href:"/resume.png",
+      href:"/resume1.pdf",
       download: true,
     },
   ];
@@ -49,7 +49,7 @@ const Contact = () => {
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white min-h-screen"
     >
       <div className="flex flex-col justify-center max-w-screen-lg mx-auto h-full ">
-        <div className="pb-8">
+        <div className="pb-8 m-4">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
